2f0668609f746be97f01fd79a515a24b7679d1e2
Botond Osváth <osvathbotond25@gmail.com>
2021-06-08 23:25:17 +0200

    Applied Dániel's comments (fixed 2)

--- cpu/norms_cpu.cpp
+++ cpu/norms_cpu.cpp
@@ -3,51 +3,60 @@
 #include <cmath>
 
 template<typename T1, typename T2>
-double lp_sum(std::vector<T1> *vec, T2 p, int i_start, int i_end) {
+double lp_sum(const std::vector<T1>& vec, T2 p, int i_start, int i_end) {
     double sum = 0;
     for (int i = i_start; i < i_end; i++) {
-        sum += std::pow(std::abs((*vec)[i]), p);
+        sum += std::pow(std::abs(vec[i]), p);
     }
     return sum;
 }
 
 template<typename T1, typename T2>
-double serial_lp(std::vector<T1> *vec, int n, T2 p) {
+double serial_lp(const std::vector<T1>& vec, int n, T2 p) {
     double sum = lp_sum(vec, p, 0, n);
     double norm = std::pow(sum, 1.0 / p);
     return norm;
 }
 
 template<typename T1, typename T2>
-double parallel_lp(std::vector<T1> *vec, int n, T2 p, int number_of_threads) {
+double parallel_lp(const std::vector<T1>& vec, int n, T2 p, int number_of_threads) {
     // If we are using only 1 thread, we should go with serial
     if (number_of_threads == 1) {
-        return serial_lp(vec, n, p);
+        auto start_cpu = std::chrono::steady_clock::now();
+        auto res = serial_lp(vec, n, p);
+        auto finish_cpu = std::chrono::steady_clock::now();
+        double elapsed_seconds_cpu = std::chrono::duration_cast<std::chrono::duration<double>>(finish_cpu - start_cpu).count();
+        return elapsed_seconds_cpu;
     }
 
     std::vector<std::future<double>> futures;
-	futures.reserve(number_of_threads);
+    futures.reserve(number_of_threads);
 
     int delta = n / number_of_threads;
     int i_start = 0;
     int i_end = delta;
 
+    auto start_cpu = std::chrono::steady_clock::now();
     for (int i = 0; i < number_of_threads; i++) {
         if (i == number_of_threads - 1) {
             i_end = n;
         }
         futures.push_back(std::async(std::launch::async,
-                                     [](std::vector<T1> *vec, T2 p, int i0, int i1) { return lp_sum(vec, p, i0, i1); },
-                                     vec, p, i_start, i_end)); //lp_sum, vec, p, i_start, i_end
+            [](const std::vector<T1>& vec, T2 p, int i0, int i1) { return lp_sum(vec, p, i0, i1); },
+            std::cref(vec), p, i_start, i_end)); //lp_sum, vec, p, i_start, i_end
 
         i_start += delta;
         i_end += delta;
     }
 
     double sum = 0;
-    for (auto &&fut:futures) {
+    for (auto&& fut : futures) {
         sum += fut.get();
     }
     double norm = std::pow(sum, 1.0 / p);
-    return norm;
+
+    auto finish_cpu = std::chrono::steady_clock::now();
+    double elapsed_seconds_cpu = std::chrono::duration_cast<std::chrono::duration<double>>(finish_cpu - start_cpu).count();
+
+    return elapsed_seconds_cpu;
 }
--- cpu/test.cpp
+++ cpu/test.cpp
@@ -26,13 +26,13 @@ void generate_double_times(int min_threads, int max_threads, int total_runs, int
 
         std::random_device rd;
         std::mt19937 e2(rd());
-        std::uniform_real_distribution<> dist(0, 25);
+        std::uniform_real_distribution<> dist(-1.0, 1.0);
         for (int i = 0; i < vector_length; i++) {
             vec[i] = dist(e2);
         }
         for (int thread = min_threads; thread <= max_threads; thread++) {
             auto start = std::chrono::steady_clock::now();
-            auto res = parallel_lp(&vec, vector_length, p, thread);
+            auto res = parallel_lp(vec, vector_length, p, thread);
             auto finish = std::chrono::steady_clock::now();
             double elapsed_seconds = std::chrono::duration_cast<
                     std::chrono::duration<double> >(finish - start).count();
@@ -65,14 +65,14 @@ void generate_float_times(int min_threads, int max_threads, int total_runs, int 
 
         std::random_device rd;
         std::mt19937 e2(rd());
-        std::uniform_real_distribution<> dist(0, 25);
+        std::uniform_real_distribution<> dist(-1.0f, 1.0f);
         for (int i = 0; i < vector_length; i++) {
             vec[i] = float(dist(e2));
         }
 
         for (int thread = min_threads; thread <= max_threads; thread++) {
             auto start = std::chrono::steady_clock::now();
-            auto res = parallel_lp(&vec, vector_length, p, thread);
+            auto res = parallel_lp(vec, vector_length, p, thread);
             auto finish = std::chrono::steady_clock::now();
             double elapsed_seconds = std::chrono::duration_cast<
                     std::chrono::duration<double> >(finish - start).count();
@@ -91,7 +91,7 @@ int main() {
     const int min_threads = 1;
     const int max_threads = 12;
     const int total_runs = 100;
-    const int vector_length = 1000000;
+    const int vector_length = 1'000'000;
     for (int i = 1; i <= 5; i ++) {
         generate_double_times(min_threads, max_threads, total_runs, vector_length, i);
         std::cout << "double " << i << " is done. \n";
