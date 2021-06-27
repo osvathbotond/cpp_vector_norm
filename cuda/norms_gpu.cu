5fb6ee09bd2a0555430777ea66ebd7a8b9269b3a
Botond Osváth <osvathbotond25@gmail.com>
2021-06-27 11:27:21 +0200

    Improved the code based on Dániel's comment.

--- cuda/norms_gpu.cu
+++ cuda/norms_gpu.cu
@@ -25,8 +25,8 @@
 static const int NUM_THREADS = 512;
 
 template<typename T>
-__global__ void sum_reduction_double(double* vec, double* res, const int n, const bool power, T p) {
-    __shared__ double partial_sum[NUM_THREADS];
+__global__ void sum_reduction(const T* vec, T* res, const int n, const bool power, T p) {
+    __shared__ T partial_sum[NUM_THREADS];
 
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     if (tid < n) {
@@ -59,64 +59,30 @@ __global__ void sum_reduction_double(double* vec, double* res, const int n, cons
     }
 
     if (threadIdx.x == 0) {
-        res[blockIdx.x] = partial_sum[0];
-    }
-
-}
-
-template<typename t>
-__global__ void sum_reduction_float(float* vec, float* res, const int n, const bool power, t p) {
-    __shared__ float partial_sum[NUM_THREADS];
-
-    int tid = blockIdx.x * blockDim.x + threadIdx.x;
-    if (tid < n) {
-        if (power) {
-            partial_sum[threadIdx.x] = pow(abs(vec[tid]), p);
+        if (gridDim.x == 1) {
+            res[blockIdx.x] = pow(partial_sum[0], (T)(1.0/p));
         }
         else {
-            partial_sum[threadIdx.x] = vec[tid];
+            res[blockIdx.x] = partial_sum[0];
         }
-    }
-    else {
-        partial_sum[threadIdx.x] = 0;
-    }
-
-    // Sync the threads to have all of the needed data in the shared memory
-    __syncthreads();
-
-    // Do the reduction (example with 8 numbers):
-    // a               b       c   d   e f g h
-    // a+e             b+f     c+g d+h e f g h
-    // a+e+c+g         b+f+c+g c+g d+h e f g h
-    // a+e+c+g+b+f+c+g b+f+c+g c+g d+h e f g h
-    // And the result is just the 0-th element
-    for (int s = blockDim.x / 2; s > 0; s >>= 1) {
-        if (threadIdx.x < s) {
-            partial_sum[threadIdx.x] += partial_sum[threadIdx.x + s];
-        }
-        // We do need to wait for all of the threads to do the sums
-        __syncthreads();
-    }
-
-    if (threadIdx.x == 0) {
-        res[blockIdx.x] = partial_sum[0];
+        
     }
 
 }
 
 template<typename T>
-double gpu_lp(double* vec, int vector_length, T p) {
+T gpu_lp(T* vec, int vector_length, T p) {
     // Host-side variables
-    std::vector <double> pows(vector_length);
-    double res;
+    std::vector <T> pows(vector_length);
+    T res;
 
-    size_t bytes = vector_length * sizeof(double);
+    size_t bytes = vector_length * sizeof(T);
 
     // ceil(vector_length / NUM_THREADS)
     int NUM_BLOCKS = (vector_length + NUM_THREADS - 1) / NUM_THREADS;
 
     // Pointers to the device-side variables
-    double* d_vec, * d_res;
+    T *d_vec, *d_res1, *d_res2;
 
     // Allocate the memory on the GPU and move the vector (with error handling)
     cudaError_t err = cudaSuccess;
@@ -126,7 +92,13 @@ double gpu_lp(double* vec, int vector_length, T p) {
         return -1;
     }
 
-    err = cudaMalloc(&d_res, bytes);;
+    err = cudaMalloc(&d_res1, bytes);
+    if (err != cudaSuccess) {
+        std::cout << "Error allocating CUDA memory: " << cudaGetErrorString(err) << "\n";
+        return -1;
+    }
+
+    err = cudaMalloc(&d_res2, bytes);
     if (err != cudaSuccess) {
         std::cout << "Error allocating CUDA memory: " << cudaGetErrorString(err) << "\n";
         return -1;
@@ -140,7 +112,7 @@ double gpu_lp(double* vec, int vector_length, T p) {
 
     // The first sum-reduction. Each block gives back a number, so the first NUM_BLOCKS elements
     // of the result d_res will have the needed information for us (the partial sums).
-    sum_reduction_double << <NUM_BLOCKS, NUM_THREADS >> > (d_vec, d_res, vector_length, true, p);
+    sum_reduction << <NUM_BLOCKS, NUM_THREADS >> > (d_vec, d_res1, vector_length, true, p);
     err = cudaGetLastError();
     if (err != cudaSuccess) {
         std::cout << "CUDA error in kernel call (during sum reduction): " << cudaGetErrorString(err) << "\n";
@@ -150,10 +122,19 @@ double gpu_lp(double* vec, int vector_length, T p) {
     // Since a reduction gives us back NUM_BLOCKS elements, we need to do it until NUM_BLOCKS == 1.
     int left;
     int NUM_BLOCKS_RED = NUM_BLOCKS;
+    int source_counter = 1;
     do {
         left = NUM_BLOCKS_RED;
         NUM_BLOCKS_RED = (left + NUM_THREADS - 1) / NUM_THREADS;
-        sum_reduction_double << <NUM_BLOCKS_RED, NUM_THREADS >> > (d_res, d_res, left, false, 0);
+        if (source_counter == 1) {
+            sum_reduction << <NUM_BLOCKS_RED, NUM_THREADS >> > (d_res1, d_res2, left, false, p);
+            source_counter = 2;
+        }
+        else {
+            sum_reduction << <NUM_BLOCKS_RED, NUM_THREADS >> > (d_res2, d_res1, left, false, p);
+            source_counter = 1;
+        }
+
         err = cudaGetLastError();
         if (err != cudaSuccess) {
             std::cout << "CUDA error in kernel call (during sum reduction): " << cudaGetErrorString(err) << "\n";
@@ -162,7 +143,12 @@ double gpu_lp(double* vec, int vector_length, T p) {
     } while (NUM_BLOCKS_RED > 1);
 
     // Copying back to the host (only one number; the 0-th element of the d_res), with error handling.
-    err = cudaMemcpy(&res, d_res, sizeof(double), cudaMemcpyDeviceToHost);
+    if (source_counter == 1) {
+        err = cudaMemcpy(&res, d_res1, sizeof(T), cudaMemcpyDeviceToHost);
+    }
+    else {
+        err = cudaMemcpy(&res, d_res2, sizeof(T), cudaMemcpyDeviceToHost);
+    }
     if (err != cudaSuccess) {
         std::cout << "Error copying memory to host: " << cudaGetErrorString(err) << "\n";
         return -1;
@@ -175,121 +161,84 @@ double gpu_lp(double* vec, int vector_length, T p) {
         return -1;
     }
 
-    err = cudaFree(d_res);
+    err = cudaFree(d_res1);
     if (err != cudaSuccess) {
         std::cout << "Error freeing allocation: " << cudaGetErrorString(err) << "\n";
         return -1;
     }
 
-    double norm = std::pow(res, 1.0 / p);
+    err = cudaFree(d_res2);
+    if (err != cudaSuccess) {
+        std::cout << "Error freeing allocation: " << cudaGetErrorString(err) << "\n";
+        return -1;
+    }
 
-    return norm;
+    return res;
 }
 
 template<typename T>
-double gpu_lp(float* vec, int vector_length, T p) {
-    // Host-side variables
-    std::vector <float> pows(vector_length);
-    float res;
+void one_vector_test(int vector_length, T p, T error) {
+    std::vector <T> vec(vector_length);
 
-    size_t bytes = vector_length * sizeof(float);
-
-    // ceil(vector_length / NUM_THREADS)
-    int NUM_BLOCKS = (vector_length + NUM_THREADS - 1) / NUM_THREADS;
-
-    // Pointers to the device-side variables
-    float* d_vec, * d_res;
-
-    // Allocate the memory on the GPU and move the vector (with error handling)
-    cudaError_t err = cudaSuccess;
-    err = cudaMalloc(&d_vec, bytes);
-    if (err != cudaSuccess) {
-        std::cout << "Error allocating CUDA memory: " << cudaGetErrorString(err) << "\n";
-        return -1;
+    for (int i = 0; i < vector_length; i++) {
+        vec[i] = (T)1;
     }
 
-    err = cudaMalloc(&d_res, bytes);;
-    if (err != cudaSuccess) {
-        std::cout << "Error allocating CUDA memory: " << cudaGetErrorString(err) << "\n";
-        return -1;
+    auto res_gpu = gpu_lp(vec.data(), vector_length, p);
+    auto res_corr = std::pow(vector_length, (T)(1.0 / p));
+
+    if (std::abs(res_corr - res_gpu) < error) {
+        std::cout << "The one-vector test with length " << vector_length << " and p = " << p << " was successful." << std::endl;
     }
-
-    err = cudaMemcpy(d_vec, vec, bytes, cudaMemcpyHostToDevice);
-    if (err != cudaSuccess) {
-        std::cout << "Error copying memory to device: " << cudaGetErrorString(err) << "\n";
-        return -1;
+    else {
+        std::cout << "The random vector test with length " << vector_length << " and p = " << p << " failed." << std::endl;
+        std::cout << "The GPU result: " << res_gpu << std::endl;
+        std::cout << "The analytical reference: " << res_corr << std::endl;
+        std::cout << "The absolute difference: " << std::abs(res_gpu - res_corr) << std::endl;
     }
-
-    // The first sum-reduction. Each block gives back a number, so the first NUM_BLOCKS elements
-    // of the result d_res will have the needed information for us (the partial sums).
-    sum_reduction_float << <NUM_BLOCKS, NUM_THREADS >> > (d_vec, d_res, vector_length, true, p);
-    err = cudaGetLastError();
-    if (err != cudaSuccess) {
-        std::cout << "CUDA error in kernel call (during sum reduction): " << cudaGetErrorString(err) << "\n";
-        return -1;
-    }
-
-    // Since a reduction gives us back NUM_BLOCKS elements, we need to do it until NUM_BLOCKS == 1.
-    int left;
-    int NUM_BLOCKS_RED = NUM_BLOCKS;
-    do {
-        left = NUM_BLOCKS_RED;
-        NUM_BLOCKS_RED = (left + NUM_THREADS - 1) / NUM_THREADS;
-        sum_reduction_float << <NUM_BLOCKS_RED, NUM_THREADS >> > (d_res, d_res, left, false, 0);
-        err = cudaGetLastError();
-        if (err != cudaSuccess) {
-            std::cout << "CUDA error in kernel call (during sum reduction): " << cudaGetErrorString(err) << "\n";
-            return -1;
-        }
-    } while (NUM_BLOCKS_RED > 1);
-
-    // Copying back to the host (only one number; the 0-th element of the d_res), with error handling.
-    err = cudaMemcpy(&res, d_res, sizeof(float), cudaMemcpyDeviceToHost);
-    if (err != cudaSuccess) {
-        std::cout << "Error copying memory to host: " << cudaGetErrorString(err) << "\n";
-        return -1;
-    }
-
-    // Freeing the memory on the device. Not doing so can cause memory-leak, with error handling.
-    err = cudaFree(d_vec);
-    if (err != cudaSuccess) {
-        std::cout << "Error freeing allocation: " << cudaGetErrorString(err) << "\n";
-        return -1;
-    }
-
-    err = cudaFree(d_res);
-    if (err != cudaSuccess) {
-        std::cout << "Error freeing allocation: " << cudaGetErrorString(err) << "\n";
-        return -1;
-    }
-
-    double norm = std::pow(res, 1.0 / p);
-
-    return norm;
 }
 
-int main() {
-    const int vector_length = 511*11*17;
-    const int p = 1;
+template<typename T>
+void random_vector_test(int vector_length, T p, T error, int cpu_threads) {
+    std::vector <T> vec(vector_length);
 
-    std::vector <float> vec(vector_length);
-
-    // Not the most effective way of creating a random vector.
     std::random_device rd;
     std::mt19937 e2(rd());
-    std::uniform_real_distribution<> dist(-1.0f, 1.0f);
+    std::uniform_real_distribution<> dist(1.0, 1.0);
     for (int i = 0; i < vector_length; i++) {
-        vec[i] = float(dist(e2));
+        vec[i] = (T)(dist(e2));
     }
 
-    // Not the most accurate way of timing the process.
-    auto start = std::chrono::steady_clock::now();
-    auto res = gpu_lp(vec.data(), vector_length, p);
-    auto finish = std::chrono::steady_clock::now();
-    double elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(finish - start).count();
+    auto res_gpu = gpu_lp(vec.data(), vector_length, p);
+    auto res_cpu = parallel_lp(vec, vector_length, p, cpu_threads);
 
-    std::cout << "Calculating the " << p << "-norm of a(n) " << vector_length << " long random float vector on the GPU took " << elapsed_seconds << " seconds." << std::endl;
-    std::cout << "The result is: " << res;
+    if (std::abs(res_cpu - res_gpu) < error) {
+        std::cout << "The random vector test with length " << vector_length << " and p = " << p << " was successful." << std::endl;
+    }
+    else {
+        std::cout << "The random vector test with length " << vector_length << " and p = " << p << " failed." << std::endl;
+        std::cout << "The GPU result: " << res_gpu << std::endl;
+        std::cout << "The CPU reference: " << res_cpu << std::endl;
+        std::cout << "The absolute difference: " << std::abs(res_gpu - res_cpu) << std::endl;
+    }
+
+}
+
+
+int main() {
+    std::cout << "Validating the GPU calculations..." <<std::endl;
+    
+    one_vector_test(100'000, 1.0f, 1e-4f);
+    one_vector_test(100'000, 2.0, 1e-6);
+    
+    for (int i = 1; i < 10; i++) {
+        if (i % 2 == 0) {
+            random_vector_test(1'000'000, (float)i, 1e-4f, 12);
+        }
+        else {
+            random_vector_test(10'000'000, (double)i, 1e-6, 12);
+        }
+    }
 
     return 0;
 
