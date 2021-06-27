1bc44d66038873ae9d99e25f40ae0a43caa05d5e
Botond Osváth <osvathbotond25@gmail.com>
2021-06-27 13:23:50 +0200

    New timing with the code from the main branch

--- cuda/norms_cpu.cpp
+++ cuda/norms_cpu.cpp
@@ -2,8 +2,8 @@
 #include <future>
 #include <cmath>
 
-template<typename T1, typename T2>
-double lp_sum(const std::vector<T1>& vec, T2 p, int i_start, int i_end) {
+template<typename T>
+double lp_sum(const std::vector<T>& vec, T p, int i_start, int i_end) {
     double sum = 0;
     for (int i = i_start; i < i_end; i++) {
         sum += std::pow(std::abs(vec[i]), p);
@@ -11,22 +11,25 @@ double lp_sum(const std::vector<T1>& vec, T2 p, int i_start, int i_end) {
     return sum;
 }
 
-template<typename T1, typename T2>
-double serial_lp(const std::vector<T1>& vec, int n, T2 p) {
+template<typename T>
+double serial_lp(const std::vector<T>& vec, int n, T p) {
     double sum = lp_sum(vec, p, 0, n);
     double norm = std::pow(sum, 1.0 / p);
     return norm;
 }
 
-template<typename T1, typename T2>
-double parallel_lp(const std::vector<T1>& vec, int n, T2 p, int number_of_threads) {
+template<typename T>
+double parallel_lp(const std::vector<T>& vec, int n, T p, int number_of_threads) {
     // If we are using only 1 thread, we should go with serial
     if (number_of_threads == 1) {
-        auto start_cpu = std::chrono::steady_clock::now();
-        auto res = serial_lp(vec, n, p);
-        auto finish_cpu = std::chrono::steady_clock::now();
-        double elapsed_seconds_cpu = std::chrono::duration_cast<std::chrono::duration<double>>(finish_cpu - start_cpu).count();
+        auto start_cpu = std::chrono::high_resolution_clock::now();
+        auto norm = serial_lp(vec, n, p);
+        auto finish_cpu = std::chrono::high_resolution_clock::now();
+        double elapsed_seconds_cpu = std::chrono::duration_cast<std::chrono::microseconds>(finish_cpu - start_cpu).count() * 0.001;
         return elapsed_seconds_cpu;
+
+        // The return removed; we are returning the calculation time instead
+        // return norm;
     }
 
     std::vector<std::future<double>> futures;
@@ -36,13 +39,13 @@ double parallel_lp(const std::vector<T1>& vec, int n, T2 p, int number_of_thread
     int i_start = 0;
     int i_end = delta;
 
-    auto start_cpu = std::chrono::steady_clock::now();
+    auto start_cpu = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < number_of_threads; i++) {
         if (i == number_of_threads - 1) {
             i_end = n;
         }
         futures.push_back(std::async(std::launch::async,
-            [](const std::vector<T1>& vec, T2 p, int i0, int i1) { return lp_sum(vec, p, i0, i1); },
+            [](const std::vector<T>& vec, T p, int i0, int i1) { return lp_sum(vec, p, i0, i1); },
             std::cref(vec), p, i_start, i_end)); //lp_sum, vec, p, i_start, i_end
 
         i_start += delta;
@@ -55,8 +58,11 @@ double parallel_lp(const std::vector<T1>& vec, int n, T2 p, int number_of_thread
     }
     double norm = std::pow(sum, 1.0 / p);
 
-    auto finish_cpu = std::chrono::steady_clock::now();
-    double elapsed_seconds_cpu = std::chrono::duration_cast<std::chrono::duration<double>>(finish_cpu - start_cpu).count();
+    auto finish_cpu = std::chrono::high_resolution_clock::now();
+    double elapsed_seconds_cpu = std::chrono::duration_cast<std::chrono::microseconds>(finish_cpu - start_cpu).count() * 0.001;
 
     return elapsed_seconds_cpu;
+
+    // The return removed; we are returning the calculation time instead
+    // return norm;
 }
--- cuda/norms_gpu.cu
+++ cuda/norms_gpu.cu
@@ -25,8 +25,8 @@
 static const int NUM_THREADS = 512;
 
 template<typename T>
-__global__ void sum_reduction_double(double* vec, double* res, const int n, const bool power, T p) {
-    __shared__ double partial_sum[NUM_THREADS];
+__global__ void sum_reduction(const T* vec, T* res, const int n, const bool power, T p) {
+    __shared__ T partial_sum[NUM_THREADS];
 
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     if (tid < n) {
@@ -59,121 +59,108 @@ __global__ void sum_reduction_double(double* vec, double* res, const int n, cons
     }
 
     if (threadIdx.x == 0) {
-        res[blockIdx.x] = partial_sum[0];
-    }
-
-}
-
-template<typename t>
-__global__ void sum_reduction_float(float* vec, float* res, const int n, const bool power, t p) {
-    __shared__ float partial_sum[NUM_THREADS];
-
-    int tid = blockIdx.x * blockDim.x + threadIdx.x;
-    if (tid < n) {
-        if (power) {
-            partial_sum[threadIdx.x] = pow(abs(vec[tid]), p);
+        if (gridDim.x == 1) {
+            res[blockIdx.x] = pow(partial_sum[0], (T)(1.0 / p));
         }
         else {
-            partial_sum[threadIdx.x] = vec[tid];
+            res[blockIdx.x] = partial_sum[0];
         }
+
+    }
+
+}
+
+template<typename T>
+T gpu_lp(T* vec, int vector_length, T p) {
+    // Host-side variables
+    std::vector <T> pows(vector_length);
+    T res;
+
+    size_t bytes = vector_length * sizeof(T);
+
+    // ceil(vector_length / NUM_THREADS)
+    int NUM_BLOCKS = (vector_length + NUM_THREADS - 1) / NUM_THREADS;
+
+    // Pointers to the device-side variables
+    T* d_vec, * d_res1, * d_res2;
+
+    // Cuda event for the device-side timing
+    cudaEvent_t evt[2];
+    for (auto& e : evt) {
+        cudaEventCreate(&e);
+    }
+
+    // Allocate the memory on the GPU and move the vector (with error handling)
+    cudaError_t err = cudaSuccess;
+    err = cudaMalloc(&d_vec, bytes);
+    if (err != cudaSuccess) {
+        std::cout << "Error allocating CUDA memory: " << cudaGetErrorString(err) << "\n";
+        return -1;
+    }
+
+    err = cudaMalloc(&d_res1, bytes);
+    if (err != cudaSuccess) {
+        std::cout << "Error allocating CUDA memory: " << cudaGetErrorString(err) << "\n";
+        return -1;
+    }
+
+    err = cudaMalloc(&d_res2, bytes);
+    if (err != cudaSuccess) {
+        std::cout << "Error allocating CUDA memory: " << cudaGetErrorString(err) << "\n";
+        return -1;
+    }
+
+    err = cudaMemcpy(d_vec, vec, bytes, cudaMemcpyHostToDevice);
+    if (err != cudaSuccess) {
+        std::cout << "Error copying memory to device: " << cudaGetErrorString(err) << "\n";
+        return -1;
+    }
+
+    // The first event, to time only the calculations
+    cudaEventRecord(evt[0]);
+
+    // The first sum-reduction. Each block gives back a number, so the first NUM_BLOCKS elements
+    // of the result d_res will have the needed information for us (the partial sums).
+    sum_reduction << <NUM_BLOCKS, NUM_THREADS >> > (d_vec, d_res1, vector_length, true, p);
+    err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        std::cout << "CUDA error in kernel call (during sum reduction): " << cudaGetErrorString(err) << "\n";
+        return -1;
+    }
+
+    // Since a reduction gives us back NUM_BLOCKS elements, we need to do it until NUM_BLOCKS == 1.
+    int left;
+    int NUM_BLOCKS_RED = NUM_BLOCKS;
+    int source_counter = 1;
+    do {
+        left = NUM_BLOCKS_RED;
+        NUM_BLOCKS_RED = (left + NUM_THREADS - 1) / NUM_THREADS;
+        if (source_counter == 1) {
+            sum_reduction << <NUM_BLOCKS_RED, NUM_THREADS >> > (d_res1, d_res2, left, false, p);
+            source_counter = 2;
+        }
+        else {
+            sum_reduction << <NUM_BLOCKS_RED, NUM_THREADS >> > (d_res2, d_res1, left, false, p);
+            source_counter = 1;
+        }
+
+        err = cudaGetLastError();
+        if (err != cudaSuccess) {
+            std::cout << "CUDA error in kernel call (during sum reduction): " << cudaGetErrorString(err) << "\n";
+            return -1;
+        }
+    } while (NUM_BLOCKS_RED > 1);
+
+    // The second event, to time only the calculations
+    cudaEventRecord(evt[1]);
+
+    // Copying back to the host (only one number; the 0-th element of the d_res), with error handling.
+    if (source_counter == 1) {
+        err = cudaMemcpy(&res, d_res1, sizeof(T), cudaMemcpyDeviceToHost);
     }
     else {
-        partial_sum[threadIdx.x] = 0;
+        err = cudaMemcpy(&res, d_res2, sizeof(T), cudaMemcpyDeviceToHost);
     }
-
-    // Sync the threads to have all of the needed data in the shared memory
-    __syncthreads();
-
-    // Do the reduction (example with 8 numbers):
-    // a               b       c   d   e f g h
-    // a+e             b+f     c+g d+h e f g h
-    // a+e+c+g         b+f+c+g c+g d+h e f g h
-    // a+e+c+g+b+f+c+g b+f+c+g c+g d+h e f g h
-    // And the result is just the 0-th element
-    for (int s = blockDim.x / 2; s > 0; s >>= 1) {
-        if (threadIdx.x < s) {
-            partial_sum[threadIdx.x] += partial_sum[threadIdx.x + s];
-        }
-        // We do need to wait for all of the threads to do the sums
-        __syncthreads();
-    }
-
-    if (threadIdx.x == 0) {
-        res[blockIdx.x] = partial_sum[0];
-    }
-
-}
-// float, because it returns the time and not the result (it's double on the main branch)
-template<typename T>
-float gpu_lp(double* vec, int vector_length, T p) {
-    // Host-side variables
-    std::vector <double> pows(vector_length);
-    double res;
-
-    size_t bytes = vector_length * sizeof(double);
-
-    // ceil(vector_length / NUM_THREADS)
-    int NUM_BLOCKS = (vector_length + NUM_THREADS - 1) / NUM_THREADS;
-
-    // Pointers to the device-side variables
-    double* d_vec, * d_res;
-
-    // Cuda event for the device-side timing
-    cudaEvent_t evt[2];
-    for (auto& e : evt) {
-        cudaEventCreate(&e);
-    }
-
-    // Allocate the memory on the GPU and move the vector (with error handling)
-    cudaError_t err = cudaSuccess;
-    err = cudaMalloc(&d_vec, bytes);
-    if (err != cudaSuccess) {
-        std::cout << "Error allocating CUDA memory: " << cudaGetErrorString(err) << "\n";
-        return -1;
-    }
-
-    err = cudaMalloc(&d_res, bytes);;
-    if (err != cudaSuccess) {
-        std::cout << "Error allocating CUDA memory: " << cudaGetErrorString(err) << "\n";
-        return -1;
-    }
-
-    err = cudaMemcpy(d_vec, vec, bytes, cudaMemcpyHostToDevice);
-    if (err != cudaSuccess) {
-        std::cout << "Error copying memory to device: " << cudaGetErrorString(err) << "\n";
-        return -1;
-    }
-
-    // The first event, to time only the calculations
-    cudaEventRecord(evt[0]);
-
-    // The first sum-reduction. Each block gives back a number, so the first NUM_BLOCKS elements
-    // of the result d_res will have the needed information for us (the partial sums).
-    sum_reduction_double << <NUM_BLOCKS, NUM_THREADS >> > (d_vec, d_res, vector_length, true, p);
-    err = cudaGetLastError();
-    if (err != cudaSuccess) {
-        std::cout << "CUDA error in kernel call (during sum reduction): " << cudaGetErrorString(err) << "\n";
-        return -1;
-    }
-
-    // Since a reduction gives us back NUM_BLOCKS elements, we need to do it until NUM_BLOCKS == 1.
-    int left = (int)std::ceil(vector_length / (1.0 * NUM_THREADS));
-    int NUM_BLOCKS_RED = (int)std::ceil(NUM_BLOCKS / (1.0 * NUM_THREADS));
-    while (left > 1) {
-        sum_reduction_double << <NUM_BLOCKS_RED, NUM_THREADS >> > (d_res, d_res, left, false, 0);
-        err = cudaGetLastError();
-        if (err != cudaSuccess) {
-            std::cout << "CUDA error in kernel call (during sum reduction): " << cudaGetErrorString(err) << "\n";
-            return -1;
-        }
-        left = (int)std::ceil(left / (1.0 * NUM_THREADS));
-        NUM_BLOCKS_RED = (int)std::ceil(NUM_BLOCKS_RED / (1.0 * NUM_THREADS));
-    }
-    // The second event, to time only the calculations
-    cudaEventRecord(evt[1]);
-
-    // Copying back to the host (only one number; the 0-th element of the d_res), with error handling.
-    err = cudaMemcpy(&res, d_res, sizeof(double), cudaMemcpyDeviceToHost);
     if (err != cudaSuccess) {
         std::cout << "Error copying memory to host: " << cudaGetErrorString(err) << "\n";
         return -1;
@@ -186,13 +173,17 @@ float gpu_lp(double* vec, int vector_length, T p) {
         return -1;
     }
 
-    err = cudaFree(d_res);
+    err = cudaFree(d_res1);
     if (err != cudaSuccess) {
         std::cout << "Error freeing allocation: " << cudaGetErrorString(err) << "\n";
         return -1;
     }
 
-    double norm = std::pow(res, 1.0 / p);
+    err = cudaFree(d_res2);
+    if (err != cudaSuccess) {
+        std::cout << "Error freeing allocation: " << cudaGetErrorString(err) << "\n";
+        return -1;
+    }
 
     // Wait for the event evt[1]. This is redundant.
     cudaEventSynchronize(evt[1]);
@@ -202,107 +193,11 @@ float gpu_lp(double* vec, int vector_length, T p) {
     cudaEventElapsedTime(&dt, evt[0], evt[1]);
 
     return dt;
+
+    // The return removed; we are returning the calculation time instead
+    // return res;
 }
-// float, because it returns the time and not the result (it's double on the main branch)
-template<typename T>
-float gpu_lp(float* vec, int vector_length, T p) {
-    // Host-side variables
-    std::vector <float> pows(vector_length);
-    float res;
 
-    size_t bytes = vector_length * sizeof(float);
-
-    // ceil(vector_length / NUM_THREADS)
-    int NUM_BLOCKS = (vector_length + NUM_THREADS - 1) / NUM_THREADS;
-
-    // Pointers to the device-side variables
-    float* d_vec, * d_res;
-
-    // Cuda event for the device-side timing
-    cudaEvent_t evt[2];
-    for (auto& e : evt) {
-        cudaEventCreate(&e);
-    }
-
-    // Allocate the memory on the GPU and move the vector (with error handling)
-    cudaError_t err = cudaSuccess;
-    err = cudaMalloc(&d_vec, bytes);
-    if (err != cudaSuccess) {
-        std::cout << "Error allocating CUDA memory: " << cudaGetErrorString(err) << "\n";
-        return -1;
-    }
-
-    err = cudaMalloc(&d_res, bytes);;
-    if (err != cudaSuccess) {
-        std::cout << "Error allocating CUDA memory: " << cudaGetErrorString(err) << "\n";
-        return -1;
-    }
-
-    err = cudaMemcpy(d_vec, vec, bytes, cudaMemcpyHostToDevice);
-    if (err != cudaSuccess) {
-        std::cout << "Error copying memory to device: " << cudaGetErrorString(err) << "\n";
-        return -1;
-    }
-
-    // The first event, to time only the calculations
-    cudaEventRecord(evt[0]);
-
-    // The first sum-reduction. Each block gives back a number, so the first NUM_BLOCKS elements
-    // of the result d_res will have the needed information for us (the partial sums).
-    sum_reduction_float << <NUM_BLOCKS, NUM_THREADS >> > (d_vec, d_res, vector_length, true, p);
-    err = cudaGetLastError();
-    if (err != cudaSuccess) {
-        std::cout << "CUDA error in kernel call (during sum reduction): " << cudaGetErrorString(err) << "\n";
-        return -1;
-    }
-
-    // Since a reduction gives us back NUM_BLOCKS elements, we need to do it until NUM_BLOCKS == 1.
-    int left = (int)std::ceil(vector_length / (1.0 * NUM_THREADS));
-    int NUM_BLOCKS_RED = (int)std::ceil(NUM_BLOCKS / (1.0 * NUM_THREADS));
-    while (left > 1) {
-        sum_reduction_float<<<NUM_BLOCKS_RED, NUM_THREADS>>>(d_res, d_res, left, false, 0);
-        err = cudaGetLastError();
-        if (err != cudaSuccess) {
-            std::cout << "CUDA error in kernel call (during sum reduction): " << cudaGetErrorString(err) << "\n";
-            return -1;
-        }
-        left = (int)std::ceil(left / (1.0 * NUM_THREADS));
-        NUM_BLOCKS_RED = (int)std::ceil(NUM_BLOCKS_RED / (1.0 * NUM_THREADS));
-    }
-    // The second event, to time only the calculations
-    cudaEventRecord(evt[1]);
-
-    // Copying back to the host (only one number; the 0-th element of the d_res), with error handling.
-    err = cudaMemcpy(&res, d_res, sizeof(float), cudaMemcpyDeviceToHost);
-    if (err != cudaSuccess) {
-        std::cout << "Error copying memory to host: " << cudaGetErrorString(err) << "\n";
-        return -1;
-    }
-
-    // Freeing the memory on the device. Not doing so can cause memory-leak.
-    err = cudaFree(d_vec);
-    if (err != cudaSuccess) {
-        std::cout << "Error freeing allocation: " << cudaGetErrorString(err) << "\n";
-        return -1;
-    }
-
-    err = cudaFree(d_res);
-    if (err != cudaSuccess) {
-        std::cout << "Error freeing allocation: " << cudaGetErrorString(err) << "\n";
-        return -1;
-    }
-
-    double norm = std::pow(res, 1.0 / p);
-
-    // Wait for the event evt[1]. This is redundant.
-    cudaEventSynchronize(evt[1]);
-
-    //Calculating the time
-    float dt = 0.0f;
-    cudaEventElapsedTime(&dt, evt[0], evt[1]);
-
-    return dt;
-}
 
 template<typename T>
 void generate_double_times(int cpu_threads, int total_runs, int vector_length, T p) {
@@ -312,7 +207,7 @@ void generate_double_times(int cpu_threads, int total_runs, int vector_length, T
     std::string fn = std::string("double_p") + std::to_string(p) + std::string("_n") +
         std::to_string(vector_length) + std::string(".dat");
     output.open(fn);
-    output << "# The first column contains only the calculation times (power and sum done on the cpu) with " << cpu_threads << "threads, the second "
+    output << "# The first column contains only the calculation times in ms (power and sum done on the cpu), with " << cpu_threads << "threads, the second "
            << "column contains the total cpu time with the same threads, the third column contains the total gpu times (with copy, malloc, etc) and the "
            <<"fourth column contains only the calculation times (power and sum done on the gpu) on the gpu. \n";
     for (int run = 0; run < total_runs; run++) {
@@ -327,19 +222,18 @@ void generate_double_times(int cpu_threads, int total_runs, int vector_length, T
         }
 
         // Calculating the total times, on the host-side
-        auto start_cpu = std::chrono::steady_clock::now();
-        auto elapsed_seconds_cpu_calc = parallel_lp(vec, vector_length, p, cpu_threads);
-        auto finish_cpu = std::chrono::steady_clock::now();
-        double elapsed_seconds_cpu_total = std::chrono::duration_cast<std::chrono::duration<double>>(finish_cpu - start_cpu).count();
+        auto start_cpu = std::chrono::high_resolution_clock::now();
+        auto elapsed_ms_cpu_calc = parallel_lp(vec, vector_length, (double)p, cpu_threads);
+        auto finish_cpu = std::chrono::high_resolution_clock::now();
+        auto elapsed_ms_cpu_total = std::chrono::duration_cast<std::chrono::microseconds>(finish_cpu - start_cpu).count() * 0.001;
 
-        auto start_gpu = std::chrono::steady_clock::now();
-        auto res_gpu = gpu_lp(vec.data(), vector_length, p);
-        auto finish_gpu = std::chrono::steady_clock::now();
-        double elapsed_seconds_gpu_calc = res_gpu / 1000.0;
-        double elapsed_seconds_gpu_total = std::chrono::duration_cast<std::chrono::duration<double>>(finish_gpu - start_gpu).count();
+        auto start_gpu = std::chrono::high_resolution_clock::now();
+        auto elapsed_ms_gpu_calc = gpu_lp(vec.data(), vector_length, (double)p);
+        auto finish_gpu = std::chrono::high_resolution_clock::now();
+        auto elapsed_ms_gpu_total = std::chrono::duration_cast<std::chrono::microseconds>(finish_gpu - start_gpu).count() * 0.001;
 
 
-        output << elapsed_seconds_cpu_calc << ", " << elapsed_seconds_cpu_total << ", " << elapsed_seconds_gpu_total << ", " << elapsed_seconds_gpu_calc << "\n";
+        output << elapsed_ms_cpu_calc << ", " << elapsed_ms_cpu_total << ", " << elapsed_ms_gpu_total << ", " << elapsed_ms_gpu_calc << "\n";
     }
     output.close();
 }
@@ -352,7 +246,7 @@ void generate_float_times(int cpu_threads, int total_runs, int vector_length, T 
     std::string fn = std::string("float_p") + std::to_string(p) + std::string("_n") +
         std::to_string(vector_length) + std::string(".dat");
     output.open(fn);
-    output << "# The first column contains only the calculation times (power and sum done on the cpu) with " << cpu_threads << "threads, the second "
+    output << "# The first column contains only the calculation times  in ms (power and sum done on the cpu), with " << cpu_threads << "threads, the second "
            << "column contains the total cpu time with the same threads, the third column contains the total gpu times (with copy, malloc, etc) and the "
            << "fourth column contains only the calculation times (power and sum done on the gpu) on the gpu. \n";
     for (int run = 0; run < total_runs; run++) {
@@ -367,19 +261,18 @@ void generate_float_times(int cpu_threads, int total_runs, int vector_length, T 
         }
 
         // Calculating the total times, on the host-side
-        auto start_cpu = std::chrono::steady_clock::now();
-        auto elapsed_seconds_cpu_calc = parallel_lp(vec, vector_length, p, cpu_threads);
-        auto finish_cpu = std::chrono::steady_clock::now();
-        double elapsed_seconds_cpu_total = std::chrono::duration_cast<std::chrono::duration<double>>(finish_cpu - start_cpu).count();
+        auto start_cpu = std::chrono::high_resolution_clock::now();
+        auto elapsed_ms_cpu_calc = parallel_lp(vec, vector_length, (float)p, cpu_threads);
+        auto finish_cpu = std::chrono::high_resolution_clock::now();
+        auto elapsed_ms_cpu_total = std::chrono::duration_cast<std::chrono::microseconds>(finish_cpu - start_cpu).count() * 0.001;
 
-        auto start_gpu = std::chrono::steady_clock::now();
-        auto res_gpu = gpu_lp(vec.data(), vector_length, p);
-        auto finish_gpu = std::chrono::steady_clock::now();
-        double elapsed_seconds_gpu_calc = res_gpu / 1000.0;
-        double elapsed_seconds_gpu_total = std::chrono::duration_cast<std::chrono::duration<double>>(finish_gpu - start_gpu).count();
+        auto start_gpu = std::chrono::high_resolution_clock::now();
+        auto elapsed_ms_gpu_calc = gpu_lp(vec.data(), vector_length, (float)p);
+        auto finish_gpu = std::chrono::high_resolution_clock::now();
+        auto elapsed_ms_gpu_total = std::chrono::duration_cast<std::chrono::microseconds>(finish_gpu - start_gpu).count() * 0.001;
 
 
-        output << elapsed_seconds_cpu_calc <<", " << elapsed_seconds_cpu_total << ", " << elapsed_seconds_gpu_total << ", " << elapsed_seconds_gpu_calc << "\n";
+        output << elapsed_ms_cpu_calc << ", " << elapsed_ms_cpu_total << ", " << elapsed_ms_gpu_total << ", " << elapsed_ms_gpu_calc << "\n";
     }
     output.close();
 }
